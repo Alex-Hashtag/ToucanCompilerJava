e044e47ac6bf340b5b44d2c2a1ac740b0f873192
amihaylov25 <aleksmihailov2015@gmail.com>
2024-10-29 12:30:03 +0200

    Started TOML support, and made a basic toucan project to test parsing later

--- src/main/java/org/alex_hashtag/Main.java
+++ src/main/java/org/alex_hashtag/Main.java
@@ -1,17 +1,22 @@
 package org.alex_hashtag;
 
 import org.alex_hashtag.tokenization.TokenStream;
+import org.tomlj.Toml;
+import org.tomlj.TomlArray;
+import org.tomlj.TomlParseResult;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 
 public class Main
 {
-    public static void main(String[] args)
-    {
-        new TokenStream("""
-                \"
-                \"jfirnr\"
-                
-                \"
-                """).printTokens();
+    public static void main(String[] args) throws IOException {
+        Path source = Paths.get("test_project/rainforest.toml");
+        TomlParseResult result = Toml.parse(source);
+        result.errors().forEach(System.err::println);
+
     }
 }
