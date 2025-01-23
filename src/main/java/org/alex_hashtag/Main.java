19af84a986eb9e2c95dc0dc3c838dc569596ca68
Alex-Hashtag <121322412+Alex-Hashtag@users.noreply.github.com>
2025-01-23 21:46:07 +0200

    Converted project to Gradle

--- src/main/java/org/alex_hashtag/Main.java
+++ src/main/java/org/alex_hashtag/Main.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.stream.Stream;
-
+////ssss
 
 @CommandLine.Command(
         name = "rainforest",
